9f76a1dcc24294aba51c862744f9ab9679e7fa02
Tushar <tushartalekar2024@gmail.com>
2025-09-14 15:04:22 +0530

    Frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-amber-500 h-20 w-20 flex items-center justify-center'>
+      
+      App
+    </div>
+  )
+}
+
+export default App
